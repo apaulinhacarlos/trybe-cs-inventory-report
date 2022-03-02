83a0e636f88899a49f977ac98d1c8f89d65e4693
aPaulinhaCarlos <apaulinhacarlos@gmail.com>
2022-03-02 12:58:08 -0300

    resolve requisito 3

---
+++ inventory_report/inventory/inventory.py
@@ -0,0 +1,20 @@
+import csv
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
+
+
+class Inventory:
+    @classmethod
+    def read_csv(cls, file_path, report_type):
+        with open(file_path) as file:
+            file_content = [data for data in csv.DictReader(file)]
+            if report_type == "simples":
+                return SimpleReport.generate(file_content)
+            elif report_type == "completo":
+                return CompleteReport.generate(file_content)
+            return list(file_content)
+
+    @classmethod
+    def import_data(cls, file_path, report_type):
+        if ".csv" in file_path:
+            return cls.read_csv(file_path, report_type)
