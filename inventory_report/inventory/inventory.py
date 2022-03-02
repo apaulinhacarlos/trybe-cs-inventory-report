f3ed6553cfac2656ce887bd707317315f579f62b
aPaulinhaCarlos <apaulinhacarlos@gmail.com>
2022-03-02 12:59:54 -0300

    resolve requisito 4

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,4 +1,5 @@
 import csv
+import json
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
@@ -15,6 +16,18 @@ class Inventory:
             return list(file_content)
 
     @classmethod
+    def read_json(cls, file_path, report_type):
+        with open(file_path) as file:
+            file_content = [data for data in json.load(file)]
+            if report_type == "simples":
+                return SimpleReport.generate(file_content)
+            elif report_type == "completo":
+                return CompleteReport.generate(file_content)
+            return list(file_content)
+
+    @classmethod
     def import_data(cls, file_path, report_type):
         if ".csv" in file_path:
             return cls.read_csv(file_path, report_type)
+        elif ".json" in file_path:
+            return cls.read_json(file_path, report_type)
